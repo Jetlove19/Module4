e0493d718d9b47d0392c8fd6b39781554bcbabd3
Jetlove19 <rbrorlove@gmail.com>
2022-12-19 12:13:28 +0300

    Сделано задание 4.1.18

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -70,6 +70,41 @@ namespace Module4
 
                 Console.WriteLine("Your color is cyan!");
             }
+
+            ///Задание 4.1.18
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color2 = Console.ReadLine();
+
+            switch (color2)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yeallow");
+                    break;
+            }
         }
     }
 }
